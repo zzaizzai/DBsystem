f343e6f1fbaaf188c4a602eeee8037f1e7a53d16
zzaizzai <junsai0530@gmail.com>
2022-11-17 01:04:20 +0900

    ddd

--- DBsystem.py
+++ DBsystem.py
@@ -72,7 +72,7 @@ def show_main_info(target: str):
 def show_main_info_compare(target_list:list):
     df_main = pd.read_excel(path_DB_main, sheet_name='DB', index_col=None)
     df_method = pd.read_excel(path_method, sheet_name='method', index_col=None)
-    df_target_1 = pd.read_excel(path_target, sheet_name='target', index_col=None)
+    df_target = pd.read_excel(path_target, sheet_name='target', index_col=None)
     if '-' in target_list[0]:
         target_name = target_list[0].split("-")[0][0:3]
         start_target = target_list[0].split("-")[0][-3:]
@@ -83,25 +83,77 @@ def show_main_info_compare(target_list:list):
     print(target_list)
     df_main_temp = df_main
     def get_method_name(x):
-        Service.reference_method_table(x, df_method)
-        return
-    df_target_1 = df_main_temp[df_main_temp["target"] == target_list[0]]
-    df_target_1 = df_target_1.reset_index(drop=True)
-    # df_target_1["method"] = df_target_1["method_id"].apply(get_method_name)
-    df_target_1.loc[:,target_list[0]] = df_target_1.loc[:,"value"]
-    df_target_1 = df_target_1.loc[:,['condition','type','unit',target_list[0]]]
-    print(df_target_1)
+        return Service.reference_method_table(x, df_method)
 
-    df_target_2 = df_main_temp.loc[df_main_temp["target"] == target_list[1],:]
-    df_target_1 = df_target_1.reset_index(drop=True)
-    # df_target_2["method_name"] = df_target_2["method_id"]
-    # df_target_2["method"] = df_target_2["method_id"].apply(get_method_name)
-    df_target_2[target_list[1]] = df_target_2["value"]
-    df_target_2 = df_target_2.loc[:,['condition','type','unit', target_list[1]]]
-    print(df_target_2)
+    def get_df_target(df_get_df_target, target:str):
+        df_get_df_target = df_get_df_target[df_get_df_target["target"] == target]
+        df_get_df_target = df_get_df_target.reset_index(drop=True)
+        df_get_df_target["method"] = df_get_df_target["method_id"].apply(get_method_name)
+        df_get_df_target.loc[:,target] = df_get_df_target.loc[:,"value"]
+        # df_target = df_target.loc[:,['method_id','method','condition','type','unit', target]]
+        # print(df_target.to_markdown())
+        return df_get_df_target
 
-    df_merge_yoko = pd.merge(df_target_1,df_target_2, on=['method','condition','type','unit'], how='outer')
-    print(df_merge_yoko.to_markdown())
+    def merge_df_col(df_old, df_new):
+
+        # print(df_new)
+        # print(df_old)
+        for row_index in df_new.index:
+            # print(df_new.loc[[row_index],:])
+            # print(df_new.at[row_index, 'method_id'])
+            if len(df_old[(df_old['method_id'] == df_new.at[row_index, 'method_id']) 
+                        & (df_old['condition'] == df_new.at[row_index, 'condition'])
+                        & (df_old['type'] == df_new.at[row_index, 'type'])
+                        & (df_old['unit'] == df_new.at[row_index, 'unit'])]) > 0:
+                # print('add')
+                index_of_row_old = df_old[(df_old['method_id'] == df_new.at[row_index, 'method_id']) 
+                        & (df_old['condition'] == df_new.at[row_index, 'condition'])
+                        & (df_old['type'] == df_new.at[row_index, 'type'])
+                        & (df_old['unit'] == df_new.at[row_index, 'unit'])].index.to_list()[0]
+                target_temp = df_new.at[row_index, "target"]
+
+                df_old.at[index_of_row_old, target_temp] = df_new.at[row_index, 'value']
+                
+            else:
+                df_old = pd.concat([df_old, df_new.loc[[row_index],:]])
+                # print('new add')
+            
+        return df_old
+        
+
+
+    df_show_target = pd.DataFrame({
+                            'method_id': pd.Series(dtype='int'),
+                           'method': pd.Series(dtype='int'),
+                           'condition': pd.Series(dtype='int'),
+                           'type': pd.Series(dtype='str'),
+                           'unit': pd.Series(dtype='str'),})
+    for a_target in target_list:
+        df_target = get_df_target(df_main, a_target)
+        df_show_target = merge_df_col(df_show_target, df_target)
+
+    def reference_target_list(target_list:list):
+        df_target = pd.read_excel(path_target, sheet_name='target', index_col=None)
+        df_show_target_list_info = pd.DataFrame()
+        for a_target_list in target_list:
+            print(a_target_list)
+            if len(df_target[df_target["target"] == a_target_list]) > 0:
+                target_information = df_target[df_target["target"] == a_target_list]
+                df_show_target_list_info = pd.concat([df_show_target_list_info, target_information], axis=0)
+            else:
+                pass
+        return df_show_target_list_info
+        
+
+    print(reference_target_list(target_list).to_markdown())
+    # print(df_show_target.to_markdown())
+    df_show_target = df_show_target.drop(['target','value'], axis=1)
+    df_show_target = df_show_target.sort_values(['method_id'])
+    print(df_show_target.to_markdown())
+        
+
+    # print(df_show_target.to_markdown())
+    # print(df_merge_yoko.to_markdown())
     # print(df_target_2)
     # df_target.loc[str(target_list[0])] = df_target.loc[:,["value"]]
 
@@ -109,15 +161,15 @@ if __name__ == "__main__":
     print('DB system')
     # target = input('input target: ')
     # show_main_info(target)
-    target_list = []
-    while True:
-        print('current target list: ',target_list)
-        a = input(f'target_list: ')
-        if a == 'go':
-            break
-        elif a== 'remove':
-            target_list.pop()
-        else:
-            target_list.append(a)
+    target_list = ['CBA001-003']
+    # while True:
+    #     print('current target list: ',target_list)
+    #     a = input(f'target_list: ')
+    #     if a == 'go':
+    #         break
+    #     elif a== 'remove':
+    #         target_list.pop()
+    #     else:
+    #         target_list.append(a)
 
     show_main_info_compare(target_list)
